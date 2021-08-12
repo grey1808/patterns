abedc8a3c4477236de3e43c1c89e71a7b1f350de
grey1808 <44221929+grey1808@users.noreply.github.com>
2021-08-12 09:32:56 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/* Шаблон Фабричный*/
+use controller\FabricController\Ford;
+use controller\FabricController\Lada;
+
+/* Шаблон Фабричный*/
+include "controller/FabricPatternController.php";
+new Ford('Focus 1', 'Белый');
+new Lada('Calina', 'Красный');
